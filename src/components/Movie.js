82720e5dac8f074c93c252d02d7a5b269a630ffd
aqeeliqbal <aqeeliqbal@gmail.com>
2021-12-30 02:10:02 +0100

    Movie Component Refactored. changed to class component instead of Hooks

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { useParams } from "react-router-dom";
 //config
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
@@ -6,8 +6,9 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
 //hook
-import { useMovieFetch } from "../Hooks/useMovieFetch";
+//import { useMovieFetch } from "../Hooks/useMovieFetch";   //not used because refectored to class components
 
+import API from '../API'
 //image
 import NoImage from '../images/no_image.jpg';
 import Spinner from "./Spinner";
@@ -15,33 +16,76 @@ import MovieInfoBar from "./MovieInfoBar";
 import Grid from "./Grid";
 import Actor from "./Actor";
 
-export const Movie = () => {
-    const {movieId} = useParams(); // movieId is the name of param used in App.js for rout
-    const {state: movie, loading, error} =  useMovieFetch(movieId);
-    if(loading ) return <Spinner/>;
-    if(error) return<div>Something Went Wrong...</div>
-    return(
-    <>
-        <BreadCrumb movieTitle={movie.title}/>
-        <MovieInfo movie={movie}/>
-        <MovieInfoBar 
-            time={movie.runtime} 
-            budget={movie.budget} 
-            revenue={movie.revenue} 
-        />
-        <Grid header='Actors'>
-            {movie.actors.map(actor => (
-                <Actor
-                    key={actor.credit_id}
-                    name={actor.name}
-                    character={actor.character}
-                    imageUrl = {actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage}
-                />
-            ))}
-        </Grid>
-    </>
-    );
+class Movie extends Component {
+    //const {movieId} = useParams(); // movieId is the name of param used in App.js for rout
+    //const {state: movie, loading, error} =  useMovieFetch(movieId);
+    state = {
+        movie: {},
+        loading: true,
+        error: false,
+    };
+
+    fetchMovie  = async () => {
+        const {movieId} = this.props.params;
+        try{
+            this.setState({error: false, loading: true});
+            
+            const movie = await API.fetchMovie(movieId);
+            const credits = await API.fetchCredits(movieId)
+
+            //get directors only 
+            const directors = credits.crew.filter( member => member.job === 'Director');
+
+            this.setState({
+                movie: {
+                    ...movie,
+                    actors: credits.cast,
+                    directors,
+                },
+                loading: false,
+            });
+        }
+        catch (error){
+            this.setState({error: true, loading: false});
+        }
+    }
+
+    componentDidMount(){
+        this.fetchMovie();
+    }
+    render(){
+        const {movie, loading, error} = this.state;
+
+        if(loading ) return <Spinner/>;
+        if(error) return<div>Something Went Wrong...</div>
+        return(
+        <>
+            <BreadCrumb movieTitle={movie.title}/>
+            <MovieInfo movie={movie}/>
+            <MovieInfoBar 
+                time={movie.runtime} 
+                budget={movie.budget} 
+                revenue={movie.revenue} 
+            />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl = {actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage}
+                    />
+                ))}
+            </Grid>
+        </>
+        );
+    }
+
 };
 
+// React-router 6 doesn't support class components, so "useParams" hook of react-router-dom will not work
+// to overcome that we created below functional wrapper component.
+const MovieWithParams = props => <Movie {...props} params={useParams()} />
+export default MovieWithParams; //
 
-export default Movie;
+//export default Movie;
